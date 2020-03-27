9c6149ecb64745c733d6fe477b99f381866aed1c
fuxiao <2642458318@qq.com>
2020-03-27 12:14:26 +0800

    创建vue项目

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,76 @@
 <template>
-    $END$
+    <div>
+        <el-form :rules="rules" ref="loginForm" :model="loginForm" class="loginContainer">
+            <h3 class="loginTitle">系统登录</h3>
+            <el-form-item prop="username">
+                <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入用户名"></el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+                <el-input type="text" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
+            </el-form-item>
+            <!--是否记住密码-->
+            <el-checkbox v-model="checked" class="loginRemember"></el-checkbox>
+            <el-button type="primary" style="width: 100%;" @click="submitLogin">登录</el-button>
+        </el-form>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Login"
+        name: "Login",
+        data() {
+            return {
+                loginForm: {
+                    username: 'admin',
+                    password: '123'
+                },
+                checked: true,
+                rules: {
+                    username: [{required: true, message: '请输入用户名', trigger: 'blur'}],
+                    password: [{required: true, message: '请输入密码', trigger: 'blur'}]
+                },
+
+            }
+        },
+        methods: {
+            submitLogin() {
+                this.$refs.loginForm.validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        //console.log('error submit!!');
+                        this.$message.error('请输入所有信息');
+                        return false;
+                    }
+                });
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style>
+    .loginContainer {
+        border-radius: 15px;
+        background-clip: padding-box;
+        margin: 180px auto;
+        width: 350px;
+        padding: 35px 35px 15px 35px;
+        background: #fff;
+        border: 1px solid #eaeaea;
+        box-shadow: 0 0 25px #cac6c6;
+    }
+
+    .loginTitle {
+        /*位置：上右下左*/
+        margin: 0px auto 40px auto;
+        /*位置：居中*/
+        text-align: center;
+        color: black;
+    }
+
+    .loginRemember {
+        text-align: left;
+        margin: 0px 0px 20px 0px;
+    }
 
 </style>
